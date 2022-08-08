1c78db3a93e6fbd3d57269e97094d70a33065d23
N4rral <mr.shaw215@seznam.cz>
2022-08-08 17:20:13 +0200

    Version 0.1
    The program only shows the image adresses. Didn't quite manage to make up for the invalid adresses yet. Image downloading is not implemented.

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
+import random
 import cloudscraper
-scraper = cloudscraper.create_scraper()
-file = open("url.txt","w",encoding="utf-8")
-url = scraper.get("https://prnt.sc/123457").text
-index = 0
-switch = False
-img_url = ""
-for i in url:
-    index += 1
-    if index > url.find('<img class="no-click screenshot-image" src="') + 44 and i != '"':
-        img_url += i
-    elif index <= url.find('<img class="no-click screenshot-image" src="') + 44:
-        pass
-    else:
-        break
-print(img_url)
-file.write(scraper.get("https://prnt.sc/123457").text)
+
+repeat = True
+amount = 0
+while repeat:
+    try:
+        amount = int(input("Enter a number: "))
+        repeat = False
+    except:
+        print("invalid input")
+        repeat = True
+
+url_template = "https://prnt.sc/"
+add = True
+while add:
+    for i in range(0, amount):
+        scraper = cloudscraper.create_scraper()
+        url_scrape = scraper.get(url_template + str(random.randint(0, 999999))).text
+        index = 0
+        img_url = ""
+        for j in url_scrape:
+            index += 1
+            if index > url_scrape.find('<img class="no-click screenshot-image" src="') + 44 and j != '"':
+                img_url += j
+            elif index <= url_scrape.find('<img class="no-click screenshot-image" src="') + 44:
+                pass
+            else:
+                break
+        index = 0
+        url_check = ""
+        passes = 0
+        for j in img_url:
+            index += 1
+            if index <= 16:
+                url_check += j
+            else:
+                pass
+        if url_check == "//st.prntscr.com" or "imageshack" in img_url:
+            passes += 1
+        else:
+            print(img_url)
+        if passes > 0:
+            add = True
+            amount = passes
+        else:
+            add = False
