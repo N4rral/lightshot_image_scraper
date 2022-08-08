ff7a152fda345a953c9d232e794bf300fdfed4f3
N4rral <mr.shaw215@seznam.cz>
2022-08-08 21:48:29 +0200

    version 0.2
    The code now generates full amount of desired adresses. Downloading still work in progress.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import cloudscraper
 
+
+
 repeat = True
 amount = 0
 while repeat:
@@ -12,36 +14,30 @@ while repeat:
         repeat = True
 
 url_template = "https://prnt.sc/"
-add = True
-while add:
-    for i in range(0, amount):
-        scraper = cloudscraper.create_scraper()
-        url_scrape = scraper.get(url_template + str(random.randint(0, 999999))).text
-        index = 0
-        img_url = ""
-        for j in url_scrape:
-            index += 1
-            if index > url_scrape.find('<img class="no-click screenshot-image" src="') + 44 and j != '"':
-                img_url += j
-            elif index <= url_scrape.find('<img class="no-click screenshot-image" src="') + 44:
-                pass
-            else:
-                break
-        index = 0
-        url_check = ""
-        passes = 0
-        for j in img_url:
-            index += 1
-            if index <= 16:
-                url_check += j
-            else:
-                pass
-        if url_check == "//st.prntscr.com" or "imageshack" in img_url:
-            passes += 1
+i = 0
+while i < amount:
+    i += 1
+    scraper = cloudscraper.create_scraper()
+    url_scrape = scraper.get(url_template + str(random.randint(0, 999999))).text
+    img_url = ""
+    index = 0
+    for j in url_scrape:
+        index += 1
+        if index > url_scrape.find('<img class="no-click screenshot-image" src="') + 44 and j != '"':
+            img_url += j
+        elif index <= url_scrape.find('<img class="no-click screenshot-image" src="') + 44:
+            pass
         else:
-            print(img_url)
-        if passes > 0:
-            add = True
-            amount = passes
+            break
+    index = 0
+    url_check = ""
+    for j in img_url:
+        index += 1
+        if index <= 16:
+            url_check += j
         else:
-            add = False
+            pass
+    if url_check == "//st.prntscr.com" or "imageshack" in img_url:
+        amount += 1
+    else:
+        print(img_url)
