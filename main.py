423c164ef60e004c3bc86b04ca2933d082fd0553
N4rral <mr.shaw215@seznam.cz>
2022-08-10 00:44:05 +0200

    version 0.4
    The program is now able to filter out images from unavailable pictures. The overal stability is improved. Added a hard limit to how many adresses you can generate.

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import cloudscraper
 import os
 import requests
 import time
+from urllib import request
 
 repeat = True
 amount = 0
 while repeat:
     try:
-        amount = int(input("Enter a number: "))
-        repeat = False
+        amount = int(input("Input the amount of images you want to download: : "))
+        if 0 < amount < 9999:
+            repeat = False
+        else:
+            print("Input value in range 0-9999")
+            repeat = True
     except:
-        print("invalid input")
+        print("Input a numerical value in range 0-9999")
         repeat = True
 
 url_template = "https://prnt.sc/"
@@ -39,16 +44,35 @@ while i < amount:
             url_check += j
         else:
             pass
-    if url_check == "//st.prntscr.com" or "imageshack" in img_url:
+    if url_check == "//st.prntscr.com" or "imageshack" in img_url\
+            or "//image.prntscr.com" in img_url:
         i -= 1
     else:
-        print(str(i) + ": " + img_url)
-        img_data = requests.get(img_url).content
-        with open(os.path.join("images", str(i) + ".jpg"), "wb") as handler:
-            handler.write(img_data)
-    et = time.time()
-    ft = et - st
-    if i < amount:
-        time.sleep(5-ft)
-    else:
-        print("process finished")
+        if "https://i.imgur.com" in img_url:
+            response = request.urlopen(img_url)
+            new_url = response.geturl()
+        else:
+            new_url = ""
+        if new_url == "https://i.imgur.com/removed.png":
+            i -= 1
+        else:
+            if i < 10:
+                print(str(i) + ":   " + img_url)
+            elif 10 <= i < 100:
+                print(str(i) + ":  " + img_url)
+            elif 100 <= i < 1000:
+                print(str(i) + ": " + img_url)
+            else:
+                print(str(i) + ":" + img_url)
+            img_data = requests.get(img_url).content
+            with open(os.path.join("images", str(i) + ".jpg"), "wb") as handler:
+                handler.write(img_data)
+            et = time.time()
+            ft = et - st
+            if i < amount:
+                if ft >= 5:
+                    time.sleep(0)
+                else:
+                    time.sleep(5 - ft)
+            else:
+                print("process finished")
