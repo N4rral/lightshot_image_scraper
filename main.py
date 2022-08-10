2f5fc701ea1abed787938e78565e9d8548d29a0b
N4rral <mr.shaw215@seznam.cz>
2022-08-10 13:03:37 +0200

    version 0.5
    The program now has fully functional menu with some work in progress functions that are not implemented yet. You are now able to terminate the program at the beginning and you also have some options at the end of the program.

--- main.py
+++ main.py
@@ -5,74 +5,114 @@ import requests
 import time
 from urllib import request
 
-repeat = True
+full_repeat = True
 amount = 0
-while repeat:
-    try:
-        amount = int(input("Input the amount of images you want to download: : "))
-        if 0 < amount < 9999:
-            repeat = False
+# User inputs
+while full_repeat:
+    repeat = True
+    while repeat:
+        option = input("1 Download new images\n2 Edit settings\n3 Remove old images\n4 End program\nchoice: ")
+        if option == "1":
+            amount_repeat = True
+            while amount_repeat:
+                try:
+                    amount = int(input("\nInput the amount of images you want to download: "))
+                    if 0 < amount < 9999:
+                        amount_repeat = False
+                        repeat = False
+                    else:
+                        print("Input value in range 0-9999")
+                        amount_repeat = True
+                except:
+                    print("Invalid input")
+                    amount_repeat = True
+        elif option == "2":
+            print("\nwork in progress\n")
+        elif option == "3":
+            print("\nwork in progress\n")
+        elif option == "4":
+            exit()
         else:
-            print("Input value in range 0-9999")
+            print("\nInvalid value\n")
             repeat = True
-    except:
-        print("Input a numerical value in range 0-9999")
-        repeat = True
 
-url_template = "https://prnt.sc/"
-i = 0
-while i < amount:
-    st = time.time()
-    i += 1
-    scraper = cloudscraper.create_scraper()
-    url_scrape = scraper.get(url_template + str(random.randint(0, 999999))).text
-    img_url = ""
-    index = 0
-    for j in url_scrape:
-        index += 1
-        if index > url_scrape.find('<img class="no-click screenshot-image" src="') + 44 and j != '"':
-            img_url += j
-        elif index <= url_scrape.find('<img class="no-click screenshot-image" src="') + 44:
-            pass
-        else:
-            break
-    index = 0
-    url_check = ""
-    for j in img_url:
-        index += 1
-        if index <= 16:
-            url_check += j
-        else:
-            pass
-    if url_check == "//st.prntscr.com" or "imageshack" in img_url\
-            or "//image.prntscr.com" in img_url:
-        i -= 1
-    else:
-        if "https://i.imgur.com" in img_url:
-            response = request.urlopen(img_url)
-            new_url = response.geturl()
-        else:
-            new_url = ""
-        if new_url == "https://i.imgur.com/removed.png":
-            i -= 1
-        else:
-            if i < 10:
-                print(str(i) + ":   " + img_url)
-            elif 10 <= i < 100:
-                print(str(i) + ":  " + img_url)
-            elif 100 <= i < 1000:
-                print(str(i) + ": " + img_url)
-            else:
-                print(str(i) + ":" + img_url)
-            img_data = requests.get(img_url).content
-            with open(os.path.join("images", str(i) + ".jpg"), "wb") as handler:
-                handler.write(img_data)
-            et = time.time()
-            ft = et - st
-            if i < amount:
-                if ft >= 5:
-                    time.sleep(0)
+    url_template = "https://prnt.sc/"
+    i = 0
+    partial_repeat = True
+    # Generate basic urls that will later be used to get images from
+    while partial_repeat:
+        while i < amount:
+            st = time.time()
+            i += 1
+            scraper = cloudscraper.create_scraper()
+            url_scrape = scraper.get(url_template + str(random.randint(0, 999999))).text
+            img_url = ""
+            index = 0
+        # Get image url from url_scrape html code
+            for j in url_scrape:
+                index += 1
+                if index > url_scrape.find('<img class="no-click screenshot-image" src="') + 44 and j != '"':
+                    img_url += j
+                elif index <= url_scrape.find('<img class="no-click screenshot-image" src="') + 44:
+                    pass
                 else:
-                    time.sleep(5 - ft)
+                    break
+            index = 0
+        # Check whether the url is blacklisted or not
+            if "//st.prntscr.com" in img_url or "imageshack" in img_url\
+                    or "//image.prntscr.com" in img_url:
+                i -= 1
             else:
-                print("process finished")
+                # Check if the url is i.imgur.com and get a redirection url in case the image has been removed
+                if "https://i.imgur.com" in img_url:
+                    response = request.urlopen(img_url)
+                    new_url = response.geturl()
+                else:
+                    new_url = ""
+                # Check if the image has been removed and issue a replacement url if it has been removed
+                if new_url == "https://i.imgur.com/removed.png":
+                    i -= 1
+                else:
+                    # So the printed urls are in a column
+                    if i < 10:
+                        print(str(i) + ":   " + img_url)
+                    elif 10 <= i < 100:
+                        print(str(i) + ":  " + img_url)
+                    elif 100 <= i < 1000:
+                        print(str(i) + ": " + img_url)
+                    else:
+                        print(str(i) + ":" + img_url)
+                    # Saving image from the url
+                    img_data = requests.get(img_url).content
+                    with open(os.path.join("images", str(i) + ".jpg"), "wb") as handler:
+                        handler.write(img_data)
+                    et = time.time()
+                    # Get process runtime and set time.sleep() for the process to be 5 s or longer if necessary
+                    ft = et - st
+                    if i < amount:
+                        if ft >= 5:
+                            time.sleep(0)
+                        else:
+                            time.sleep(5 - ft)
+                    else:
+                        print("Process finished\n")
+                        end_repeat = True
+                        while end_repeat:
+                            end_choice = input("1 Generate another " + str(amount) + " images"
+                                               "\n2 Restart the program\n3 End the program\nchoice: ")
+                            if end_choice == "1":
+                                partial_repeat = True
+                                end_repeat = False
+                                amount += amount
+                            elif end_choice == "2":
+                                partial_repeat = False
+                                full_repeat = True
+                                end_repeat = False
+                                print("")
+                            elif end_choice == "3":
+                                partial_repeat = False
+                                full_repeat = False
+                                end_repeat = False
+                            else:
+                                print("\nInvalid value\n")
+                                end_repeat = True
